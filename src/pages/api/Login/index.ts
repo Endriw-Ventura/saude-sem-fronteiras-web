037dcc43e0d778806d7cd85eae0c5316f75e74c1
Endriw Ventura <endriwventura@yahoo.com>
2025-07-03 21:56:15 -0300

    hotfix login

--- src/pages/api/Login/index.ts
+++ src/pages/api/Login/index.ts
@@ -2,6 +2,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { users } from "@/mocks/db/users";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
   const { email, password } = req.body;
 
   const user = users.find((u) => u.email === email && u.password === password);
