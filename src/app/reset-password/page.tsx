047f8f3551ba8b9a154407b858b0e71d7c243981
Endriw Ventura <endriwventura@yahoo.com>
2025-07-03 21:48:12 -0300

    hotfix de erro do tipo null

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -5,22 +5,38 @@ import CustomForm from "@/components/ui/custom-form";
 import CustomInput from "@/components/ui/custom-input";
 import { emailService } from "@/service/service-email";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 
 export default function ResetPassword() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const token = searchParams.get("token");
+  const token = searchParams?.get("token");
   const [password, setPassword] = useState("");
   const [confirmPassword, setconfirmPassword] = useState("");
+
+  useEffect(() => {
+    if (!token) {
+      toast.error("Token inválido ou ausente.");
+      router.push("/");
+    }
+  }, [token, router]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (password === confirmPassword) {
-      const result = await emailService.resetPassword(token, password);
-      if (result?.status === 200) router.push("/");
-    } else toast.error("Passwords dont match!");
+      try {
+        const result = await emailService.resetPassword(token!, password);
+        if (result?.status === 200) {
+          toast.success("Senha redefinida com sucesso!");
+          router.push("/");
+        }
+      } catch (error) {
+        toast.error("Erro ao redefinir a senha.");
+      }
+    } else {
+      toast.error("As senhas não coincidem!");
+    }
   };
 
   return (
