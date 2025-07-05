84ea35a0bba8b32e6d79dedae5b95fd88717990d
Endriw Ventura <endriwventura@yahoo.com>
2025-07-05 17:39:12 -0300

    ajuste nas permissões de post

--- src/pages/api/Login/index.ts
+++ src/pages/api/Login/index.ts
@@ -2,13 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { users } from "@/mocks/db/users";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    res.setHeader("Allow", ["POST"]);
-    return res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-
+  res.setHeader("Allow", ["POST"]);
   const { email, password } = req.body;
-
   const user = users.find((u) => u.email === email && u.password === password);
   if (!user) return res.status(401).json({ message: "Invalid credentials" });
 
