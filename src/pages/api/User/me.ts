8bee88d7bc808448cc90f651365fd61c3d2e0795
Endriw Ventura <endriwventura@yahoo.com>
2025-07-05 18:11:21 -0300

    Ajuste no id do usuário

--- src/pages/api/User/me.ts
+++ src/pages/api/User/me.ts
@@ -5,13 +5,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const authHeader = req.headers.authorization;
   if (!authHeader)
     return res.status(401).json({ message: "No token provided" });
-
   const token = authHeader.split(" ")[1];
-
   if (!token) return res.status(401).json({ message: "Malformed token" });
   const userId = token.split("_")[1];
   if (userId) {
-    const user = memoryStore.users.find((u: any) => u.id === userId);
+    const user = memoryStore.users.find((u: any) => u.id == userId);
     if (!user) return res.status(404).json({ message: "User not found" });
     return res.status(200).json(user);
   }
